4489b91efe7a0c0cda5f2211f9a4ea3e50f32598
Vandmand <ellk@itu.dk>
2026-02-24 20:10:55 +0100

    Fixed build error

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,6 @@ import SignUp from "./routes/sign-up/SignUp.tsx";
 import Submit from "./routes/submit/Submit.tsx";
 import Layout from "./routes/Layout.tsx";
 import ScrollToTop from "./common/ScrollTop.tsx";
-import AdminPanel from "./routes/admin-panel/AdminPanel.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -22,7 +21,7 @@ createRoot(document.getElementById("root")!).render(
           <Route path="/get-started" element={<GetStarted />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/submit" element={<Submit />} />
-          <Route path="/admin-panel" element={<AdminPanel />} />
+          <Route path="/admin-panel" element={<App />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/routes/admin-panel/serverWrapper.ts
+++ src/routes/admin-panel/serverWrapper.ts
@@ -1,35 +1,35 @@
-const apiUrl = "https://api.pokerbot.dk";
+// const apiUrl = "https://api.pokerbot.dk";
 
-export const healthCheck = async () => {
-  const res = await fetch(apiUrl + "/");
-  return res.status;
-};
+// export const healthCheck = async () => {
+//   const res = await fetch(apiUrl + "/");
+//   return res.status;
+// };
 
-export const getTable = async (table: number) => {
-  try {
-    const res = await fetch(apiUrl + "/data/" + table.toString());
-    return await res.json();
-  } catch (error) {
-    throw error as Error;
-  }
-};
+// export const getTable = async (table: number) => {
+//   try {
+//     const res = await fetch(apiUrl + "/data/" + table.toString());
+//     return await res.json();
+//   } catch (error) {
+//     throw error as Error;
+//   }
+// };
 
-/**
- * Loads tables
- * @param key Api key
- * @returns {unknown} TODO Find out what this is
- */
+// /**
+//  * Loads tables
+//  * @param key Api key
+//  * @returns {unknown} TODO Find out what this is
+//  */
 
-export const getTables = async (key: string) => {
-  const resp = await fetch(apiUrl + "/files", {
-    headers: {
-      "X-API-KEY": key,
-    },
-  });
+// export const getTables = async (key: string) => {
+//   const resp = await fetch(apiUrl + "/files", {
+//     headers: {
+//       "X-API-KEY": key,
+//     },
+//   });
 
-  if (resp.status != 200) {
-    throw new Error(await resp.text());
-  }
+//   if (resp.status != 200) {
+//     throw new Error(await resp.text());
+//   }
 
-  return await resp.json();
-};
+//   return await resp.json();
+// };
